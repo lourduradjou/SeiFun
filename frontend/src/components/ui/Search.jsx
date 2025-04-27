97a04db767b8861aff1176e6142a686b9be9018f
lourduradjou <rajlourdu15@gmail.com>
2025-04-27 23:39:29 +0530

    Deleted unneeded codes and added required libraries.

--- frontend/src/components/ui/Search.jsx
+++ frontend/src/components/ui/Search.jsx
@@ -11,11 +11,8 @@ export default function Search() {
     }
 
     return (
-        <form className="max-w-xl mx-auto outline-none">
-            <label
-                htmlFor="default-search"
-                className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-            >
+        <form className="max-w-xl mx-auto outline-none text-white">
+            <label htmlFor="default-search" className="mb-2 text-sm font-medium sr-only ">
                 Search
             </label>
             <div className="relative">
@@ -39,14 +36,14 @@ export default function Search() {
                 <input
                     type="search"
                     id="default-search"
-                    className="block w-full p-4 ps-10 text-sm text-gray-900 rounded-lg bg-[#111114] dark:border-gray-800 dark:placeholder-gray-400 dark:text-white"
+                    className="block w-full p-4 ps-10 text-sm rounded-lg bg-[#111114] border-gray-800 placeholder-gray-400 "
                     placeholder="Search your favorite meme coins, etc"
                     required
                     onChange={handleSearchChange} // Handle input change
                 />
                 <button
                     type="submit"
-                    className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    className=" absolute end-2.5 bottom-2.5 focus:ring-2 font-medium rounded-lg text-sm px-4 py-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
                 >
                     Search
                 </button>
